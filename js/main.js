1ce86d90c79a63d60e90266f05501c9874389778
RobertBasile <rbasi008@uottawa.ca>
2020-05-11 19:36:40 -0400

    added a timer

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 
-window.onload = function currentTime() {
-
-
+function currentTime() {
     var date = new Date();
     var hour = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
-    hour = update(hour);
+    var ampm = hour >= 12 ? 'pm' : 'am';
+    if (hour > 12 ){
+      hour = hour - 12;
+    } else if (hour == 0) {
+      hour = 12;
+    }
     min = update(min);
-    sec = update(sec);
-    document.getElementById("curr_time").innerHTML = hour + " : " + min + " : " + sec; /* adding time to the div */ 
-    var t = setTimeout(function(){ currentTime() }, 500);
+    document.getElementById("curr_time").innerHTML = hour + " : " + min + " " + ampm; /* adding time to the div */ 
+    var t = setTimeout(function(){ currentTime() }, 10);
   
   }
 
@@ -20,6 +22,7 @@ window.onload = function currentTime() {
     showTime();
     hideButton();
     showTwoButtons();
+    currentTime();
   } 
 
   function slideUp(el) {
@@ -43,19 +46,26 @@ window.onload = function currentTime() {
   function hideTime(){
     document.getElementById("curr_time").style.visibility = 'hidden';
   }
-
-
+  function showTimer(){
+    document.getElementById("box_timer").style.visibility = 'visible';
+  }
+  
+  function hideTimer(){
+    document.getElementById("box_timer").style.visibility = 'hidden';
+  }
+  
   function Timerbtn(){
     document.getElementById("Clock").style.visibility = "hidden";
   }
   function ClockBtn(){
     showTime();
+    hideTimer();
   }
   function SwitchToTimer() { 
     hideTime();
+    showTimer();
   }
  
-  
   function update(k) {
     if (k < 10) {
       return "0" + k;
@@ -64,4 +74,32 @@ window.onload = function currentTime() {
       return k;
     }
   }
+
+  var timer = document.getElementById('timing');
+  var startbtn = document.getElementById('toggle');
+  var stopbtn = document.getElementById('untoggle');
+  var resbtn  = document.getElementById('reset');
+  var watch = new myCounter(timer);
+
+  function clear(){
+    watch.reset();
+  }
+  function startTime(){
+    watch.start();
+  }
+  function stopTime(){
+    watch.stop();
+  }
   
+  startbtn.addEventListener('click',function(){
+      startTime();
+  });
+
+  stopbtn.addEventListener('click',function(){
+    stopTime();
+  });
+  resbtn.addEventListener('click', function(){
+    clear();
+  });
+
+
